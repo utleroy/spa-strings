4d9e2e7afc2a82704a86bd3ef5a222e36e9c0313
Bryan Hankins <bryanhankins@me.com>
2016-08-13 11:20:13 -0500

    added initial files

---
+++ strings.js
@@ -0,0 +1,16 @@
+function reversal() {
+
+}
+
+function alphabits() {
+
+}
+
+function palindrome() {
+
+}
+
+var testString = "";
+reversal(testString);
+alphabits(testString);
+palindrome(testString);
