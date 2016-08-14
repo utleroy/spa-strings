a462b2dac64bc824c5351ea6a9f865bda24b7571
Bryan Hankins <bryanhankins@me.com>
2016-08-14 15:14:27 -0500

    logging user input

--- strings.js
+++ strings.js
@@ -1,16 +1,27 @@
-function reversal() {
-
-}
-
-function alphabits() {
-
-}
-
-function palindrome() {
-
-}
-
+var output = document.getElementById("output");
+var input = document.getElementById("input");
+var button = document.getElementById("submit");
 var testString = "";
-reversal(testString);
-alphabits(testString);
-palindrome(testString);
+
+
+var click = button.addEventListener("click", checkText)
+
+function checkText() {
+    console.log("button clicked", input.value)
+}
+
+  // function reversal() {
+
+  // }
+
+  // function alphabits() {
+
+  // }
+
+  // function palindrome() {
+
+  // }
+
+  // reversal(testString);
+  // alphabits(testString);
+  // palindrome(testString);
